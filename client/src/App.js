dc9db5591c47ff7a4face154d32b8b54fcb7d9a5
Paras Raut <parasraut2511@gmail.com>
2023-10-07 23:34:37 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { ContextProvider } from './Context';
 import Home from './Home';
 import Room from './Room';
+import Start from './components/Start';
 
 const App = () => {
 
@@ -13,7 +14,8 @@ const App = () => {
    <ContextProvider>
      <Router>
     <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Start />} />
+    <Route path="/WaitingRoom" element={<Home />} />
     <Route path="/room/:roomID" element={<Room />} />
     </Routes>
   </Router>
